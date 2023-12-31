173305f1c98767665abda4ae25df581f747d2c6e
Torres08 <juanlutr2002@gmail.com>
2023-12-31 20:29:28 +0100

    practica 2 terminando

---
+++ Practicas/lex/ejemplos/ejemplo1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// funcion de ejemplo
+void imprimirMensaje()
+{
+    printf("! Hola, mundo!\n");
+}
+
+/*
+    comentarios
+    multilinea
+*/
+
+int main
+{
+    // Llamada a la funcion
+    imprimirMensaje();
+    return 0;
+}
