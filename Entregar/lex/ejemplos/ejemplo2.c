cea9936ef1d24f05ce5892943a295d67e4b7f66b
Torres08 <juanlutr2002@gmail.com>
2024-01-09 19:15:46 +0100

    final

---
+++ Entregar/lex/ejemplos/ejemplo2.c
@@ -0,0 +1,23 @@
+
+
+// Pepe1
+//  Pepe 1
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void imprimirMensaje() // paso1: imprime mensaje en pantalla
+{
+    printf("! Hola, mundo!\n");
+}
+
+/*
+    ahora viene el main
+*/
+
+
+int main // paso2: llamo a / la funcion
+{
+    imprimirMensaje(); 
+    return 0;
+}
